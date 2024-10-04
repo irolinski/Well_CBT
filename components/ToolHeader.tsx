2dd8f8592d4ebbbadce99214b26fc1ce94b348ac
szczek <ignacy.rolinski@gmail.com>
2024-10-04 11:03:41 +0200

    fix(ToolHeader): fix color bug, update type definitions

--- components/ToolHeader.tsx
+++ components/ToolHeader.tsx
@@ -1,15 +1,22 @@
 import Text from "./global/Text";
+import { HTMLAttributes } from "react";
 
-const ToolHeader = (props: any, bright?: boolean) => {
+interface ToolHeaderProps extends HTMLAttributes<HTMLDivElement> {
+  bright?: boolean;
+}
+
+const ToolHeader = ({ bright, ...props }: ToolHeaderProps) => {
   return (
     <Text
       {...props}
-      className="pr-[10%] text-left text-2xl"
+      className={`pr-[10%] text-left text-2xl ${props.className}`}
       style={{
         fontFamily: "KodchasanMedium",
         color: bright ? "#F5F5F5" : "#1E1E1E",
+        ...props.style,
       }}
     />
   );
 };
+
 export default ToolHeader;
