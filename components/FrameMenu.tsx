a5b54755415bb3048a9dd811a8abec94878fb210
szczek <ignacy.rolinski@gmail.com>
2024-11-07 02:09:07 +0100

    fix(FrameMenu): make logo placement responsive

--- components/FrameMenu.tsx
+++ components/FrameMenu.tsx
@@ -6,20 +6,29 @@ import { Logo } from "./Logo";
 const FrameMenu = (props: any) => {
   const windowWidth = Dimensions.get("window").width;
   const windowHeight = Dimensions.get("window").height;
-  const topFrameHeight = windowHeight / 6;
+  const topFrameHeight = windowHeight / 6.5;
   const scrollViewHeight = windowHeight - topFrameHeight;
+
+  const logoViewWidth = windowWidth * 0.85;
+  const logoViewOffsetBottom = windowHeight * 0.03;
 
   return (
     <React.Fragment>
       <View
-        className="top-0 justify-center"
+        className="absolute justify-center"
         style={{
           width: windowWidth,
           height: topFrameHeight,
           backgroundColor: "#8dbed8",
         }}
       >
-        <View className="relative mx-8 flex-row items-center justify-between">
+        <View
+          className="absolute mx-8 mt-8 flex-row items-center justify-between"
+          style={{
+            width: logoViewWidth,
+            bottom: logoViewOffsetBottom,
+          }}
+        >
           <ToolHeader className="text-3xl" bright={true}>
             {props.title}
           </ToolHeader>
