a67b9db784e350aa3881aff9e28a8edec64845ab
szczek <ignacy.rolinski@gmail.com>
2024-12-09 23:26:02 +0100

    feat: add LearnCard UI

--- app/(tabs)/Learn.tsx
+++ app/(tabs)/Learn.tsx
@@ -7,23 +7,17 @@ const Learn = () => {
   return (
     <FrameMenu title="Learn">
       <View>
-        <Text className="mb-4 text-left text-xl">Category 1</Text>
-        {/* <LearnCard
-          name={"Cognitive-Behavioral Therapy"}
-          link={""}
-          image={require("@/assets/images/card_backgrounds/journal.jpg")}
-        />
-        <LearnCard
-          name={"Cognitive-Behavioral Therapy"}
-          link={""}
-          image={require("@/assets/images/card_backgrounds/journal.jpg")}
-        />
-        <Text className="mb-4 text-left text-xl">Category 2</Text>
-        <LearnCard
-          name={"Cognitive-Behavioral Therapy"}
-          link={""}
-          image={require("@/assets/images/card_backgrounds/journal.jpg")}
-        /> */}
+        <Text className="mb-4 text-left text-xl">Featured today</Text>
+        <View className="items-center justify-center">
+          <LearnCard
+            title={"Dangers of self-diagnosis"}
+            subtitle="American Journal of Psychology"
+            time={20}
+            link={""}
+            image={require("@/assets/images/tools/ground.webp")}
+          />
+        </View>
+        <Text className="mb-4 text-left text-xl">Categories</Text>
       </View>
     </FrameMenu>
   );
--- components/LearnCard.tsx
+++ components/LearnCard.tsx
@@ -1,44 +1,97 @@
-import {
-  View,
-  Text,
-  Pressable,
-  ImageBackground,
-  ImageSourcePropType,
-} from "react-native";
-import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
+import { Image } from "expo-image";
 import { Href, router } from "expo-router";
+import React from "react";
+import {
+  ImageSourcePropType,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-const LearnCard = ({
-  name,
-  link,
-  image,
-}: {
-  name: string;
+type LearnArticleCardTypes = {
+  title: string;
+  subtitle?: string;
+  time?: number;
   link: string;
   image: ImageSourcePropType | undefined;
-}) => {
+  color?: string;
+};
+
+const LearnArticleCard = ({
+  title,
+  subtitle,
+  time,
+  link,
+  image,
+  color,
+}: LearnArticleCardTypes) => {
   return (
     <Pressable
-      className="flex-1 mb-8 h-52 rounded-lg justify-center"
+      className="mb-8 flex-1 justify-center overflow-hidden rounded-xl"
+      style={{ height: 260, width: 360 }}
       onPress={() => router.push(link as Href)}
     >
-      <ImageBackground
-        source={image}
-        resizeMode="cover"
-        className="flex-1 ounded-lg justify-center"
+      <Image source={image} className="flex-1 justify-center rounded-2xl" />
+      <View
+        className="absolute bottom-0 w-full px-4 pb-4 pt-5"
+        style={{ height: "57%", backgroundColor: "#8DBED8" }}
       >
-        <LinearGradient
-          colors={["rgba(0, 0, 0, 0.2)", "rgba(0, 0, 0, 0.8)"]}
-          className="flex-1 justify-center items-center"
-        >
-          <Text className="text-center font-bold text-2xl text-gray-100 ">
-            {name}
-          </Text>
-        </LinearGradient>
-      </ImageBackground>
+        <View className="">
+          <View style={{ height: "50%" }}>
+            <Text
+              className="text-lg"
+              style={{ color: "#FBFBFB", fontWeight: 500 }}
+            >
+              {title}
+            </Text>
+            {subtitle && (
+              <Text
+                className="my-1.5 text-base italic"
+                style={{ color: "#FBFBFB" }}
+              >
+                {subtitle}
+              </Text>
+            )}
+          </View>
+          <View
+            className="w-full flex-row justify-between"
+            style={{ height: "50%" }}
+          >
+            <View className="justify-center">
+              {time && (
+                <Text
+                  className="text-base"
+                  style={{ color: color ?? "#FBFBFB", opacity: 0.75 }}
+                >
+                  {time} min read
+                </Text>
+              )}
+            </View>
+            <View
+              className="items-center justify-center"
+              style={{ width: "50%" }}
+            >
+              <TouchableOpacity
+                className="flex-row items-center justify-center rounded-lg"
+                style={{
+                  width: "100%",
+                  height: "85%",
+                  backgroundColor: "#fbfbfb",
+                }}
+              >
+                <Text className="mx-1 text-lg">Go to article</Text>
+                <View className="mx-1">
+                  <Feather name="arrow-right" size={24} color="black" />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
     </Pressable>
   );
 };
 
-export default LearnCard;
+export default LearnArticleCard;
