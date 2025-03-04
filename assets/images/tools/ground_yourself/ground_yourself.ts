d699c894dbdb421b45e81de9b91e9f1c94332f83
irolinski <ignacy.rolinski@gmail.com>
2025-03-04 23:57:34 +0100

    feat(tools/ground_yourself/time): add page_1

--- assets/images/tools/ground_yourself/ground_yourself.ts
+++ assets/images/tools/ground_yourself/ground_yourself.ts
@@ -1,21 +1,23 @@
-import bench from "@/assets/images/tools/ground_yourself/bench.webp";
-import birds from "@/assets/images/tools/ground_yourself/birds.webp";
-import chair from "@/assets/images/tools/ground_yourself/chair.webp";
-import feelyourbody from "@/assets/images/tools/ground_yourself/feelyourbody.webp";
-import flower from "@/assets/images/tools/ground_yourself/flower.webp";
-import foot from "@/assets/images/tools/ground_yourself/foot.webp";
-import hand from "@/assets/images/tools/ground_yourself/hand.webp";
-import lamp from "@/assets/images/tools/ground_yourself/lamp.webp";
-import list from "@/assets/images/tools/ground_yourself/list.webp";
-import scanbody from "@/assets/images/tools/ground_yourself/scanbody.webp";
-import streetlamp from "@/assets/images/tools/ground_yourself/streetlamp.webp";
-import stretch from "@/assets/images/tools/ground_yourself/stretch.webp";
-import stretchfingers from "@/assets/images/tools/ground_yourself/stretchfingers.webp";
-import texture_1 from "@/assets/images/tools/ground_yourself/texture_1.webp";
-import texture_2 from "@/assets/images/tools/ground_yourself/texture_2.webp";
-import tree from "@/assets/images/tools/ground_yourself/tree.webp";
+import bench from '@/assets/images/tools/ground_yourself/bench.webp';
+import birds from '@/assets/images/tools/ground_yourself/birds.webp';
+import calendar from '@/assets/images/tools/ground_yourself/calendar.webp';
+import chair from '@/assets/images/tools/ground_yourself/chair.webp';
+import feelyourbody from '@/assets/images/tools/ground_yourself/feelyourbody.webp';
+import flower from '@/assets/images/tools/ground_yourself/flower.webp';
+import foot from '@/assets/images/tools/ground_yourself/foot.webp';
+import hand from '@/assets/images/tools/ground_yourself/hand.webp';
+import lamp from '@/assets/images/tools/ground_yourself/lamp.webp';
+import list from '@/assets/images/tools/ground_yourself/list.webp';
+import scanbody from '@/assets/images/tools/ground_yourself/scanbody.webp';
+import streetlamp from '@/assets/images/tools/ground_yourself/streetlamp.webp';
+import stretch from '@/assets/images/tools/ground_yourself/stretch.webp';
+import stretchfingers from '@/assets/images/tools/ground_yourself/stretchfingers.webp';
+import texture_1 from '@/assets/images/tools/ground_yourself/texture_1.webp';
+import texture_2 from '@/assets/images/tools/ground_yourself/texture_2.webp';
+import tree from '@/assets/images/tools/ground_yourself/tree.webp';
 
 export const groundYourselfImages = {
+  calendar: calendar,
   flower: flower,
   foot: foot,
   stretch: stretch,
