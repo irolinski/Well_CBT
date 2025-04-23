de260e5a01940ec77f8ba355523c5a755d6e2bd0
irolinski <ignacy.rolinski@gmail.com>
2025-04-24 01:22:54 +0200

    feat(tools/cda/tutorial): add assets

---
+++ assets/images/tools/tutorials/tutorials.ts
@@ -0,0 +1,13 @@
+import brain_thought_notification from "./brain_thought_notification.webp";
+import cbt_diagram from "./cbt_diagram.webp";
+import change_the_way_you_feel from "./change_the_way_you_feel.webp";
+import perception from "./perception.webp";
+
+export const tutorialImages = {
+  cda: {
+    cbt_diagram: cbt_diagram,
+    automatic_thoughts: brain_thought_notification,
+    change_the_way_you_feel: change_the_way_you_feel,
+    cognitive_distortions: perception,
+  },
+};
---
+++ components/tools/CBTDiagramSubtitledImage.tsx
@@ -0,0 +1,32 @@
+import { Image } from "expo-image";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { tutorialImages } from "@/assets/images/tools/tutorials/tutorials";
+import { Colors } from "@/constants/styles/colorTheme";
+import { SCREEN_WIDTH } from "@/constants/styles/values";
+
+const CBTDiagramSubtitledImage = () => {
+  return (
+    <React.Fragment>
+      <Image
+        style={{ width: SCREEN_WIDTH * 0.95, height: 130 }}
+        source={tutorialImages.cda.cbt_diagram}
+      />
+      <View
+        className="flex-row justify-between px-8 py-8"
+        style={{ width: SCREEN_WIDTH * 0.95 }}
+      >
+        <Text style={styles.imageSubtitle}>Situation</Text>
+        <Text style={styles.imageSubtitle}>Thought</Text>
+        <Text style={styles.imageSubtitle}>Feeling</Text>
+        <Text style={styles.imageSubtitle}>Action</Text>
+      </View>
+    </React.Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageSubtitle: { color: Colors.white, fontSize: 14, fontWeight: 800 },
+});
+
+export default CBTDiagramSubtitledImage;
