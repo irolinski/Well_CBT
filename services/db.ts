b610dd8f47f469816926165a39e221038764a42a
irolinski <ignacy.rolinski@gmail.com>
2025-05-05 15:22:53 +0200

    chore: rename and unexport DB_NAME constant

--- services/db.ts
+++ services/db.ts
@@ -1,4 +1,4 @@
 import * as SQLite from "expo-sqlite";
 
-export const dbName = "well-test-db-047";
-export const dbPromise = SQLite.openDatabaseAsync(dbName);
+const DB_NAME = "well-test-db-047";
+export const dbPromise = SQLite.openDatabaseAsync(DB_NAME);
