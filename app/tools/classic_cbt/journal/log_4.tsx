f0448f07d5daff23188a851086ec53195d95cb33
irolinski <ignacy.rolinski@gmail.com>
2025-06-13 23:59:32 +0200

    fix(tools/cbt/mood_journal): hide keyboard on bg tap

--- app/tools/classic_cbt/journal/log_4.tsx
+++ app/tools/classic_cbt/journal/log_4.tsx
@@ -1,18 +1,18 @@
-import { router } from "expo-router";
-import React from "react";
-import { useTranslation } from "react-i18next";
-import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import AdvanceButton from "@/components/AdvanceButton";
-import Frame from "@/components/Frame";
-import Text from "@/components/global/Text";
-import ToolHeader from "@/components/tools/ToolHeader";
-import ToolNav from "@/components/tools/ToolNav";
-import ToolTextInput from "@/components/tools/ToolTextInput";
-import { journal_tool } from "@/constants/models/tools/tools";
-import { SCREEN_HEIGHT } from "@/constants/styles/values";
-import { setNote } from "@/state/features/tools/journalSlice";
-import { AppDispatch, RootState } from "@/state/store";
+import { router } from 'expo-router';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import AdvanceButton from '@/components/AdvanceButton';
+import Frame from '@/components/Frame';
+import Text from '@/components/global/Text';
+import ToolHeader from '@/components/tools/ToolHeader';
+import ToolNav from '@/components/tools/ToolNav';
+import ToolTextInput from '@/components/tools/ToolTextInput';
+import { journal_tool } from '@/constants/models/tools/tools';
+import { SCREEN_HEIGHT } from '@/constants/styles/values';
+import { setNote } from '@/state/features/tools/journalSlice';
+import { AppDispatch, RootState } from '@/state/store';
 
 const TOOL_NAME = journal_tool.name;
 const CURRENT_PAGE = 4;
@@ -31,16 +31,16 @@ const Log_4 = () => {
       />
       <Frame>
         <View className="h-full">
-          <View className="py-12">
-            <ToolHeader>
-              {t(`tools.${TOOL_NAME}.exercise.page_4.header`)}
-            </ToolHeader>
-            <TouchableWithoutFeedback
-              onPress={() => {
-                Keyboard.dismiss;
-              }}
-              accessible={false}
-            >
+          <TouchableWithoutFeedback
+            onPress={() => {
+              Keyboard.dismiss();
+            }}
+            accessible={false}
+          >
+            <View className="py-12">
+              <ToolHeader>
+                {t(`tools.${TOOL_NAME}.exercise.page_4.header`)}
+              </ToolHeader>
               <View className="my-8">
                 <View className="mb-2">
                   <Text className="mr-[15%] text-left">
@@ -53,8 +53,8 @@ const Log_4 = () => {
                   />
                 </View>
               </View>
-            </TouchableWithoutFeedback>
-          </View>
+            </View>
+          </TouchableWithoutFeedback>
         </View>
       </Frame>
       <View className="bottom-16 mx-6">
