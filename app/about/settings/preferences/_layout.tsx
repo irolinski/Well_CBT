c4d6d03cfb9b6ab1bb8be3393e9b258a9ad75d1c
irolinski <ignacy.rolinski@gmail.com>
2025-07-27 17:35:35 +0200

    refactor(about/settings): remove unused routes

--- app/about/settings/preferences/_layout.tsx
+++ app/about/settings/preferences/_layout.tsx
@@ -1,10 +1,9 @@
-import { Stack } from 'expo-router';
-import React from 'react';
+import { Stack } from "expo-router";
+import React from "react";
 
 const PreferencesLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="language" options={{ headerShown: false }} />
       <Stack.Screen name="storage" options={{ headerShown: false }} />
     </Stack>
   );
